f74c86f264425f5f6f5463fb5c020a8ff71a2f21
damirios <101191365+damirios@users.noreply.github.com>
2022-03-23 16:21:10 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,48 @@
+<?php
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	$mail->setFrom('sasuke0001@mail.ru', 'Дамир');
+	$mail->addAddress('deir96@mail.ru');
+	$mail->Subject = 'Hello!';
+
+//Тело письма
+	$body = '<h1>Письмо!</h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+
+	if(trim(!empty($_POST['phone']))){
+		$body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
+	}
+
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение: </strong> '.$_POST['message'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Адрес: </strong> '.$_POST['message'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка!!!';
+	} else {
+		$message = 'Данные отправлены';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
